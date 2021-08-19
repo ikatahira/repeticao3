5172b844a0500e89b4bda444552273753da6b7ff
ikatahira <isaque.katahira@fatec.sp.gov.br>
2021-08-19 01:44:27 +0000

    Resolução repeticao 3

---
+++ ExercicioRepeticao3.java
@@ -0,0 +1,13 @@
+public class ExercicioRepeticao3{
+
+    public static void main(String args[]){
+        for(int i=1; i<=100;i++){
+            if(i%3==0){
+                System.out.println("Os múltiplos de 3: "+i);
+            }
+        }
+    }
+
+}
+
+//3. Imprima todos os múltiplos de 3, entre 1 e 100.
